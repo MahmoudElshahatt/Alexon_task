0ba752918d4fdfc72644a79da90ae3eb59d822c5
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-03-12 23:29:08 +0200

    delete app instance

--- app/src/main/java/com/alexon/alexon_task/App.kt
+++ app/src/main/java/com/alexon/alexon_task/App.kt
@@ -6,13 +6,4 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class App : Application() {
 
-    companion object {
-        lateinit var instance: App
-            private set
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
 }
