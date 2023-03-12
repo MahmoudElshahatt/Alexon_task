bca1ab9a1527093c09cc8b6a3ceae2042dee328c
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-03-13 00:00:12 +0200

    finish

--- app/src/main/java/com/alexon/alexon_task/ui/main/products/ProductsFragment.kt
+++ app/src/main/java/com/alexon/alexon_task/ui/main/products/ProductsFragment.kt
@@ -5,25 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.alexon.alexon_task.R
 import com.alexon.alexon_task.databinding.FragmentProductsBinding
 import com.alexon.alexon_task.ui.main.adapter.ProductsAdapter
-import com.alexon.alexon_task.ui.main.products.models.ProductsResponse
+import com.alexon.alexon_task.ui.main.products.models.ProductsResponse.Product
 import com.alexon.alexon_task.util.toAuthActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProductsFragment : Fragment(), ProductsAdapter.ProductClickListener {
 
     private lateinit var binding: FragmentProductsBinding
     private val viewModel: ProductsViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -92,7 +90,7 @@ class ProductsFragment : Fragment(), ProductsAdapter.ProductClickListener {
         return adapter
     }
 
-    override fun onProductClick(product: ProductsResponse.Product) {
+    override fun onProductClick(product: Product) {
         findNavController().navigate(
             ProductsFragmentDirections.actionProductsFragmentToProductDetailsFragment(product)
         )
